b4fd20edc0170464da85a1bbc4623a792c35fac4
Huy Chiến <chienhi912@gmail.com>
2024-05-27 00:48:09 +0700

    Cập nhật laại giao diện đăng nhập , đăng ký , quên mật khâủ  Bổ sung navbar , Profile

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,12 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
-import PrimeVue from 'primevue/config';
 import LoginView from '@/views/LoginView.vue';
 import HomeView from '../views/HomeView.vue';
 import RegisterView from '../views/RegisterView.vue';
 import ForgotPasswordView from "@/views/ForgotPasswordView.vue";
 import aboutView from "@/views/AboutView.vue";
  import Navbars from "@/components/Navbar.vue";
- import Profile from "@/components/Profile.vue";
+import Profile from "@/components/Profile.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import Reg from "@/components/Register.vue";
 import Fogot from "@/components/ForgotPassword.vue";
 import Login2 from "@/views/LoginView.vue";
 import Navbar from "@/components/Navbar.vue";
+import Profile from "@/components/Profile.vue";
 
 </script>
 
@@ -14,6 +15,7 @@ import Navbar from "@/components/Navbar.vue";
     <Login2/>
     <Reg/>
     <Fogot/>
+    <Profile/>
 
 
 
