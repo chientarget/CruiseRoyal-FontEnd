d909d5fda356b70c43e1e28b2e0bf121079015f3
Huy Chiến <chienhi912@gmail.com>
2024-05-30 08:01:02 +0700

    Update Tailwind , Navbar.vue

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -23,13 +23,12 @@
           <div class="flex items-center justify-center">
             <div class="space-y-3 text-center md:text-left lg:mx-12">
               <h1 class="text-2xl"> Xin Chào, <b>Huy Chiến</b>! </h1>
-              <p7>Đăng nhập lần cuối <b>12 phút trước</b> từ <b>127.0.0.1</b></p7>
+              <p>Đăng nhập lần cuối <b>12 phút trước</b> từ <b>127.0.0.1</b></p>
               <div class="flex justify-center md:block">
                 <div class="inline-flex items-center capitalize leading-none text-sm border rounded-full py-1.5 px-4 bg-blue-500 border-blue-500 text-white">
                   <span class="inline-flex justify-center items-center w- h-4 mr-2">
                     <svg viewBox="0 0 24 24" width="16" height="16" class="inline-block">
-                      <path fill="currentColor"
-                            d="M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z"></path></svg>
+                      <path fill="currentColor" d="M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z"></path></svg>
                   </span>
                   <span>Đã xác minh</span>
                 </div>
