f0f1294395fd26f47d62f096eefbfe2379fc1bc1
chientarget <chienhi912@gmail.com>
2024-05-30 17:11:41 +0700

    Chien update update fix Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,36 @@
 <template>
-  <div class="card w-full flex justify-center bg-gray-100 fixed top-0 z-50 shadow-3">
-    <Menubar :model="items" class="flex w-8/12 justify-between bg-transparent border-none xl:max-w-7xl xl:mx-auto ">
-       <template #start>
-        <img src="/Logo/Logo-Cruise-Royal.svg" alt="logo" class=" h-5rem "  @click="$router.push('/home')"/>
-       </template>
+  <div class="card flex justify-content-center w-full bg-gray-100 fixed top-0 z-50 shadow-2 ">
+
+    <Menubar :model="items" class="flex w-8/12  bg-transparent border-none xl:max-w-7xl xl:mx-auto m-auto">
+      <template #start>
+        <img src="/Logo/Logo-Cruise-Royal.svg" alt="logo" class=" h-3rem " @click="$router.push('/home')"/>
+      </template>
       <template #item="{ item, props }">
         <a v-ripple class="flex items-center " v-bind="props.action" :href="item.link">
           <span :class="item.icon"/>
           <span class="font-bold  rounded-full hover:text-green-500">{{ item.label }}</span>
         </a>
       </template>
-      <template #end>
-        <div class="flex items-center gap-2 w-full justify-end"  @click="showMenu = false">
+      <template #end  >
+        <div class="flex items-center end-0 gap-2 w-full min-w-50 " @click="showMenu = false" >
           <i class="pi pi-phone"></i>
-         <span class=" font-bold pr-3 hover:text-green-500 cursor-pointer">Liên Hệ: 0123456789</span>
-          <Avatar class="h-3rem w-3rem border-2 hover:border-green-100" image="/img_log/anh03.jpg" shape="circle" @mouseover="showMenu = true" />
-          <div v-if="showMenu" class="absolute top-20 right-30 bg-white rounded shadow p-2" @mouseover="showMenu = true" @mouseleave="showMenu = false">
+          <span class=" font-bold pr-3 hover:text-green-500 cursor-pointer"> Liên Hệ: 0123456789</span>
+          <Avatar class="h-2rem w-2rem border-2 hover:border-green-100" image="/img_log/anh03.jpg" shape="circle"
+                  @mouseover="showMenu = true"/>
+          <div v-if="showMenu" class="absolute top-20 right-30 bg-white rounded shadow p-2" @mouseover="showMenu = true"
+               @mouseleave="showMenu = false">
             <Toast class="z-50"/>
-            <div class="cursor-pointer p-2 hover:bg-black-alpha-10 rounded" @click="$router.push('/profile')">Thông tin người dùng</div>
+            <div class="cursor-pointer p-2 hover:bg-black-alpha-10 rounded" @click="$router.push('/profile')">
+              Thông tin người dùng
+            </div>
             <div class="cursor-pointer p-2 hover:bg-black-alpha-10 rounded" @click="$router.push('/')">Đăng xuất</div>
           </div>
         </div>
       </template>
     </Menubar>
   </div>
+
+
 </template>
 
 <script setup lang="ts">
@@ -34,13 +41,14 @@ interface MenuItem {
   icon?: string;
   link?: string;
 }
+
 const showMenu = ref(false);
 
 const items = ref<MenuItem[]>([
-  { label: 'Tìm du thuyền', link:'home'},
+  {label: 'Tìm du thuyền', link: 'home'},
   // { label: 'Tìm vé máy bay' , link:'home'},
-  { label: 'Doanh nghiệp', link:'home' },
-  { label: 'Blog' , link:'home'}
+  {label: 'Doanh nghiệp', link: 'home'},
+  {label: 'Blog', link: 'home'}
 ]);
 
 
