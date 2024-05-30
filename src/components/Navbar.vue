cf221de50f39c76f1dc47b8ee91e43d3a4d2e86f
Huy Chiến <chienhi912@gmail.com>
2024-05-30 11:42:58 +0700

    Update Profile and logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@
         <img src="/Logo/Logo-Cruise-Royal.svg" alt="logo" class=" h-5rem "  @click="$router.push('/home')"/>
        </template>
       <template #item="{ item, props }">
-        <a v-ripple class="flex items-center " v-bind="props.action">
+        <a v-ripple class="flex items-center " v-bind="props.action" :href="item.link">
           <span :class="item.icon"/>
           <span class="font-bold  rounded-full hover:text-green-500">{{ item.label }}</span>
         </a>
@@ -13,7 +13,7 @@
       <template #end>
         <div class="flex items-center gap-2 w-full justify-end"  @click="showMenu = false">
           <i class="pi pi-phone"></i>
-         <span class=" font-bold pr-3 hover:text-green-500 cursor-pointer">Liên Hệ: 0888888888</span>
+         <span class=" font-bold pr-3 hover:text-green-500 cursor-pointer">Liên Hệ: 0123456789</span>
           <Avatar class="h-3rem w-3rem border-2 hover:border-green-100" image="/img_log/anh03.jpg" shape="circle" @mouseover="showMenu = true" />
           <div v-if="showMenu" class="absolute top-20 right-30 bg-white rounded shadow p-2" @mouseover="showMenu = true" @mouseleave="showMenu = false">
             <div class="cursor-pointer p-2 hover:bg-black-alpha-10 rounded" @click="$router.push('/profile')">Thông tin người dùng</div>
@@ -31,13 +31,14 @@ import {ref} from 'vue';
 interface MenuItem {
   label?: string;
   icon?: string;
+  link?: string;
 }
 const showMenu = ref(false);
 
 const items = ref<MenuItem[]>([
-  { label: 'Tìm du thuyền' },
-  { label: 'Tìm vé máy bay' },
-  { label: 'Doanh nghiệp' },
-  { label: 'Blog' }
+  { label: 'Tìm du thuyền', link:'home'},
+  { label: 'Tìm vé máy bay' , link:'home'},
+  { label: 'Doanh nghiệp', link:'home' },
+  { label: 'Blog' , link:'home'}
 ]);
 </script>
