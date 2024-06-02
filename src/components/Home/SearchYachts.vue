f2cb5230885a95395d759bf9b9ae3dd524a98059
Huy Chiến <chienhi912@gmail.com>
2024-06-02 22:22:17 +0700

    Update Home

--- src/components/Home/SearchYachts.vue
+++ src/components/Home/SearchYachts.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="flex justify-center mt-24 relative">
-    <video class="max-w-6xl   rounded-3xl" src="/Video/Mixivivuduthuyen.mp4" autoplay muted loop></video>
+    <video class="max-w-6xl rounded-3xl" src="/Video/Mixivivuduthuyen.mp4" autoplay muted loop></video>
     <div class="absolute bg-white rounded-3xl shadow-lg p-6 flex flex-col items-center bottom-[-8rem] w-3/4 max-w-5xl">
       <h2 class="text-3xl font-bold mb-2">Bạn lựa chọn du thuyền Hạ Long nào?</h2>
       <p class="text-gray-600 mb-4">Hơn 100 tour du thuyền hạng sang giá tốt đang chờ bạn</p>
-      <div class="flex space-x-4 w-full">
-        <InputText type="text" class="w-2/6   rounded-3xl " placeholder="Nhập tên du thuyền"/>
-        <Dropdown class="w-2/8  rounded-3xl" :options="locations" optionLabel="name" placeholder="Tất cả địa điểm" scroll-height="1"/>
-        <Dropdown class="w-2/8  rounded-3xl" :options="prices" optionLabel="range" placeholder="Tất cả mức giá" scroll-height="1"/>
-        <Button label="Tìm kiếm" class="bg-teal-500 text-white   rounded-3xl"/>
+      <div class="flex space-x-4 w-full text-sm">
+        <InputText type="text " class="w-4/15  text-sm rounded-3xl pl-4" placeholder="Nhập tên du thuyền"/>
+        <Dropdown class="w-4/15 rounded-3xl pi pi-flag flex align-items-center px-3" :options="locations" optionLabel="name" placeholder="Tất cả địa điểm" scroll-height="1"/>
+        <Dropdown class="w-4/15  rounded-3xl pi pi-money-bill flex align-items-center px-3" :options="prices" optionLabel="range" placeholder="Tất cả mức giá" scroll-height="1"/>
+        <Button label="Tìm kiếm" class="bg-teal-500 text-white w-full  rounded-3xl"/>
       </div>
     </div>
+
+
   </div>
 </template>
 
