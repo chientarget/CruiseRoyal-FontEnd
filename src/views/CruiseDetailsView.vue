0494d93bd354e47877dd1dfd8bff9251dcf59b15
chientarget <chienhi912@gmail.com>
2024-06-07 13:11:41 +0700

    Chien push to pc

--- src/views/CruiseDetailsView.vue
+++ src/views/CruiseDetailsView.vue
@@ -5,17 +5,17 @@ import Footer from "@/components/Footer.vue";
 </script>
 <template>
   <Navbar class="w-screen z-50"/>
-  <BreadcrumbDetail class="w-screen relative top-20 z-2"/>
-  <YachtTitle class="w-screen relative top-20"/>
-  <Highlight class="w-screen relative top-20"/>
+  <BreadcrumbDetail class="w-screen relative top-20 "/>
+  <YachtTitle class="w-screen relative top-20 "/>
+  <Highlight class="w-screen relative top-20 "/>
   <Footer/>
 
 
-<!--  <div class="w-screen  relative top-24">-->
-<!--    <div class=" w-9/12 mx-auto ">-->
-<!--      <div class="container  "></div>-->
-<!--    </div>-->
-<!--  </div>-->
+  <!--  <div class="w-screen  relative top-24">-->
+  <!--    <div class=" w-9/12 mx-auto ">-->
+  <!--      <div class="container  "></div>-->
+  <!--    </div>-->
+  <!--  </div>-->
 
 </template>
 
